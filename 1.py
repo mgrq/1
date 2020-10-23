b58e4656d9d5efe93cf7f4d35559a8f8e6655cd5
Daniil Borsyakov <dborsakov@gmail.com>
2020-10-24 01:11:47 +0300

    upd 1 task

--- 1.py
+++ 1.py
@@ -1,15 +1,12 @@
-a = [1,1,2,3,5,8,3,21,34,4,89]
-n=-1
-while n<len(a):
-    n+=1
-      if n>=len(a):
-        break
-    if a[n]<5:
-        print(a[n])
+a = [1, 1, 2, 3, 5, 8, 3, 21, 34, 4, 89]
+# n = -1
+# while n < len(a)-1:
+#     n += 1
+#     # if n >= len(a):
+#     #     break
+#     if a[n] < 5:
+#         print(a[n])
 
 
-
-
-
-
-
+for i in a:
+    if i < 5: print(i)
