308752ac607fea13fc370d4e17069c072b8f4ad1
Daniil Borsyakov <dborsakov@gmail.com>
2020-10-24 01:51:39 +0300

    add 3 and 4

--- 2.py
+++ 2.py
@@ -6,3 +6,25 @@ for i in a:
         c.append(i)
 
 print(c)
+
+dic = {'name': 'vasya', 'old': '25'}
+print(dic.get('old'))
+
+# 3 задача
+x = {'a': 4, 'b': 6, 't': 9, 'f': 2, 'i': 7}
+x_sort = list(x.items())
+x_sort.sort(key = lambda i: i[1])  # по возрастанию
+print(x_sort)
+x_sort.sort(key = lambda i: i[1], reverse=True)  # по убыванию
+print(x_sort)
+
+# 4 задача
+d1 = {'a': 1, 'b': 2}
+d2 = {'c': 5, 'e': 3, 'f': 0}
+d3 = {'h': 1, 'r': 9}
+d = {}
+for i in d1,d2,d3:
+    d.update(i.items())
+print(d)
+
+
